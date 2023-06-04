e8d0144d143abdfcf7e6bebe6893990e54a0cbc8
Krasnovskiy Denis <dikrasnovskii@domclick.ru>
2023-06-04 15:04:41 +0300

    added https

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # vite_spa
+
+### For https on dev:
+
+- install [mkcert](https://github.com/FiloSottile/mkcert)
+- create a folder `ssl` in the project root
+- in `ssl` folder run command `mkcert -key-file key.pem -cert-file cert.pem spa.vite.ru`
+- run command for macOS `sudo nano /etc/hosts`
+- add line `127.0.0.1 spa.vite.ru`
+- save changes and close file
+
+### For start dev
+
+- npm ci
+- npm run dev
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
+export default defineConfig(() => {
+  return {
+    server: {
+      port: 3000,
+      host: 'spa.vite.ru',
+      https: {
+        key: 'ssl/key.pem',
+        cert: 'ssl/cert.pem'
+      },
+      open: true
+    },
+    plugins: [react()]
+  };
 });
